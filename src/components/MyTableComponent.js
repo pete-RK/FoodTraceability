a0ed3554ec8b800d1240ed95669432f3072b4f97
purushothamas <purushothamapurshi8@gmail.com>
2023-12-03 15:34:44 -0700

    Table view has been changed

--- src/components/MyTableComponent.js
+++ src/components/MyTableComponent.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import '../styles/TableStyles.css'; 
+import '../styles/TableStyles.css';
 
 const MyTableComponent = ({ data }) => {
+  // Extract column headers
+  const columnHeaders = data.map(item => item.columnName);
+
+  // Calculate the maximum number of rows
+  const maxRows = Math.max(...data.map(item => item.values.length));
+
+  // Create rows data
+  const rowsData = Array.from({ length: maxRows }).map((_, rowIndex) => {
+    return data.map(column => column.values[rowIndex] || '');
+  });
+
   return (
     <table className="data-table">
       <thead>
         <tr>
-          <th>Container Load URI</th>
+          {columnHeaders.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td>{item}</td>
+        {rowsData.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {row.map((cell, cellIndex) => (
+              <td key={cellIndex}>{cell}</td>
+            ))}
           </tr>
         ))}
       </tbody>
